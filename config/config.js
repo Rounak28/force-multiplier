f5ca0994854e66f0ce0d08c59c95424bb4a1ad34
Thinkwik <rounak@thinkwik.com>
2019-03-20 11:46:55 +0530

    minor change

--- config/config.js
+++ config/config.js
@@ -8,9 +8,7 @@ const environmentConfig = config[environment];
 const fs = require('fs');
 const finalConfig = _.merge(defaultConfig, environmentConfig);
 
-// as a best practice
-// all global variables should be referenced via global. syntax
-// and their names should always begin with g
+
 global.gConfig = finalConfig;
 global.gConfig.rootPath = path.normalize(__dirname + '/../');
 
